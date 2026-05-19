3a380150f1842f0474fd59f8d52b4e20f34c8e15
shaikabdulsakeeb-code <shaikabdulsakeeb@gmail.com>
2026-05-19 15:13:16 +0530

    fix: add connection timeouts to nodemailer transporter to prevent infinite hangs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -34,6 +34,9 @@ const sendEmail = async ({ to, subject, html }) => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, // Gmail App Password (not regular password)
     },
+    connectionTimeout: 8000, // 8 seconds timeout for TCP connection
+    greetingTimeout: 8000,   // 8 seconds timeout for SMTP greeting
+    socketTimeout: 8000,     // 8 seconds timeout for data transmission
   });
 
   // Helper to strip HTML tags to generate a clean plain-text alternative
